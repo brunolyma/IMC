f63ca3faedbbdc5de59f6f4075ab6e6353febe39
Bruno Lima <brunoadl88@gmail.com>
2023-11-16 16:41:47 +0000

    improve conditions

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-const inputWeight = document.querySelector("input#weight");
-const inputHeight = document.querySelector("input#height");
+const weight = document.querySelector("input#weight");
+const height = document.querySelector("input#height");
 const modal = document.querySelector("#result");
+const message = document.querySelector("#result span.message");
 const errorMessage = document.querySelector("section#error-form");
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log(typeof inputWeight.value);
-  if (isNaN(+inputWeight)) {
-    // errorMessage.classList.remove("hide");
-    console.log("SIm");
+  const result = IMC(weight.value, height.value);
+  message.innerText = `${result}`;
+
+  console.log(notANumber(weight.value));
+  console.log(notANumber(height.value));
+  // return;
+  if (notANumber(height.value) || notANumber(weight.value)) {
+    showErrorPopUp();
+  } else {
+    handleModal();
   }
 }
 
+window.addEventListener("focusin", handleInputfocus);
+function handleInputfocus(event) {
+  hideErrorPopUp();
+}
+
+function showErrorPopUp() {
+  errorMessage.classList.remove("hide");
+}
+function hideErrorPopUp() {
+  errorMessage.classList.add("hide");
+}
+
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
+}
+
+function notANumber(value) {
+  return isNaN(value) || value == "";
+}
+
 function handleModal() {
-  modal.classList.toggle("show");
+  modal.classList.toggle("close");
+}
+
+window.addEventListener("keydown", handleKeyDown);
+function handleKeyDown(event) {
+  if (event.key == "Escape") {
+    handleModal();
+  }
 }
